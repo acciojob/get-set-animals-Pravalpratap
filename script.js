b57895c4ee95b60d2f3963dfff23d04c54bc950c
Praval Pratap Singh <113175059+Pravalpratap@users.noreply.github.com>
2024-06-19 18:09:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,42 @@
-//complete this code
-class Animal {}
+class Animal {
+    constructor(species) {
+        this._species = species;
+    }
 
-class Dog extends Animal {}
+    // Getter for species
+    get species() {
+        return this._species;
+    }
 
-class Cat extends Animal {}
+    // Method to make a sound
+    makeSound() {
+        console.log(`The ${this.species} makes a sound`);
+    }
+}
+
+// Define the Cat class that extends Animal
+class Cat extends Animal {
+    constructor(species) {
+        super(species); // Call the parent class constructor
+    }
+
+    // Method to purr
+    purr() {
+        console.log("purr");
+    }
+}
+
+// Define the Dog class that extends Animal
+class Dog extends Animal {
+    constructor(species) {
+        super(species); // Call the parent class constructor
+    }
+
+    // Method to bark
+    bark() {
+        console.log("woof");
+    }
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
